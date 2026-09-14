221a239438b7695cd2c24699114cca947c1158e6
Jorgevan-SAJ <jorge.van@hotmail.com>
2026-09-13 21:48:57 -0300

    Update page.js

--- app/igrejas/nova/page.js
+++ app/igrejas/nova/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { supabase } from '../../../lib/supabaseClient'
+import { supabase } from '../../../lib/supabase'
 
 export default function NovaIgreja() {
   const [form, setForm] = useState({ nome: '', email: '', senha: '', termos: false })
