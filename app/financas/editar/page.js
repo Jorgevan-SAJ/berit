0d3f2acbd7449ccf5f0ad4596555bd12705d713a
Jorgevan-SAJ <jorge.van@hotmail.com>
2026-09-25 20:19:49 -0300

    Redirect to finances with selected tab after save

--- app/financas/editar/page.js
+++ app/financas/editar/page.js
@@ -120,7 +120,7 @@ export default function EditarFinancas() {
     if (error) {
       setErro(error.message || 'Não foi possível salvar o lançamento. Tente novamente.')
     } else {
-      window.location.href = '/financas'
+    window.location.href = `/financas?aba=${form.aba_id}`
     }
   }
   const campo = {
